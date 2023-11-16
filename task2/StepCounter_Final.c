cc3e718fa8cd552c528b1ba2312dfd48ca26f72a
Yifan Ren <146280681+JAcKs0n02@users.noreply.github.com>
2023-11-16 16:51:34 +0000

    Task2

--- task2/StepCounter_Final.c
+++ task2/StepCounter_Final.c
@@ -6,11 +6,7 @@
 // Struct moved to header file
 
 // Define any additional variables here
-char choice;
-int MaxAmount = 1000, recordednum = 0, i;
-char line[100];
-char* filename;
-char date[11] , time[6], steps[5];
+
 // Global variables for filename and FITNESS_DATA array
 
 
@@ -46,7 +42,70 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 
 // Complete the main function
+FITNESS_DATA records[100];
+int i;
+
+void first_1() {
+    
+}
+
+void A_CheckFilename() {
+    char inputname[] = "FitnessData_2023.csv";
+    char CheckName[100];
+    printf("Enter the Filename: ");
+        scanf("%s", CheckName);
+        if (strcmp(CheckName, inputname)){
+            printf("Error: Could not open file\n");
+        }
+        else{
+            printf("File successfully loaded.\n");
+        }
+}
+
+void B_TotalNum() {
+    first_1();
+    int recordednum = 0, i;
+    char line[100];
+    char date[11] , time[6], steps[5];
+
+    FILE *file = fopen("FitnessData_2023.csv", "r");
+        if (file == NULL) {
+            fprintf(stderr, "Error: Unable to open the file.\n");
+                
+        }
+
+
+    while(fgets(line,30,file)){
+        tokeniseRecord(line, ",", date, time, steps);
+        strcpy(records[recordednum].date, date);
+        strcpy(records[recordednum].time, time);
+        records[recordednum].steps=atoi(steps);
+        recordednum++;
+    }
+        
+    fclose(file);
+
+    printf("Total records: %d\n", recordednum);
+}
+
+void C_FewestSteps() {
+    first_1();
+    int min;
+    int n;
+    int Num;
+
+    min = records[0].steps;
+    for (n = 0; n < i; n++){
+        if (records[n].steps < min){
+            min = records[n].steps;
+            Num = n;
+        }
+    }
+    printf("Fewest steps: %s, %s\n", records[Num].date, records[Num].time);
+
+}
 int main() {
+    char choice;
     printf("MENU: \n");
     printf("A: Specify the filename to be imported\n");
     printf("B: Display the total number of records in the file\n");
@@ -60,56 +119,39 @@ int main() {
     printf("Enter your choice: ");
     scanf("%c", &choice);
 
-    FILE *file = fopen("FitnessData_2023.csv", "r");
-            if (file == NULL) {
-                fprintf(stderr, "Error: Unable to open the file.\n");
-                return 1;
-            }
-     FITNESS_DATA records[MaxAmount];
-    while(fgets(line,30,file)){
-    tokeniseRecord(line, ",", date, time, steps);
-    strcpy(records[recordednum].date, date);
-    strcpy(records[recordednum].time, time);
-    records[recordednum].steps=atoi(steps);
-    recordednum++;
-    }
-        
-    fclose(file);
-
-
     switch(choice){
         case 'A':
-            printf("Enter the Filename: ");
-            scanf("%s", filename);
-            if (filename = "FitnessData_2023.csv"){
-                printf("File successfully loaded.\n");
-            }
-            else{
-                printf("Error: Could not open file");
-            }
+        case 'a':
+            A_CheckFilename();
             break;
 
         case 'B':
-            printf("Total records: %d\n", recordednum);
+        case 'b':
+            B_TotalNum();
             break;
         
         case 'C':
-
+        case 'c':
+            C_FewestSteps();
             break;
 
         case 'D':
+        case 'd':
 
             break;
 
-        case 'E': 
+        case 'E':
+        case 'e':
 
             break;
 
-        case 'F': 
+        case 'F':
+        case 'f': 
 
             break;
 
         case 'Q':
+        case 'q':
             printf("QUIT\n");
             break;
 
