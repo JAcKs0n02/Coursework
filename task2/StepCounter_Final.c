67cea3831840ecdcdd2c0d9b06a775634635147b
Yifan Ren <146280681+JAcKs0n02@users.noreply.github.com>
2023-11-22 18:19:02 +0000

    task2

--- task2/StepCounter_Final.c
+++ task2/StepCounter_Final.c
@@ -20,7 +20,7 @@ void tokeniseRecord(const char *input, const char *delimiter,
     
     // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {        strcpy(date, token);
+    if (token != NULL) {strcpy(date, token);
     }
     
     token = strtok(NULL, delimiter);
@@ -43,13 +43,32 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 FITNESS_DATA records[100];
-int i;
+int recordednum = 0, i;
 
-void first_1() {
+void ImportedFile() {
+    FILE *file = fopen("FitnessData_2023.csv", "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Unable to open the file.\n");
+        return;
+    }
     
+    char line[100];
+    char date[11] , time[6], steps[5];
+
+    while(fgets(line,30,file)){
+    tokeniseRecord(line, ",", date, time, steps);
+    strcpy(records[recordednum].date, date);
+    strcpy(records[recordednum].time, time);
+    records[recordednum].steps=atoi(steps);
+    recordednum++;
+    }
+   
+    fclose(file);
 }
 
 void A_CheckFilename() {
+    ImportedFile();
+
     char inputname[] = "FitnessData_2023.csv";
     char CheckName[100];
     printf("Enter the Filename: ");
@@ -63,47 +82,42 @@ void A_CheckFilename() {
 }
 
 void B_TotalNum() {
-    first_1();
-    int recordednum = 0, i;
-    char line[100];
-    char date[11] , time[6], steps[5];
-
-    FILE *file = fopen("FitnessData_2023.csv", "r");
-        if (file == NULL) {
-            fprintf(stderr, "Error: Unable to open the file.\n");
-                
-        }
-
-
-    while(fgets(line,30,file)){
-        tokeniseRecord(line, ",", date, time, steps);
-        strcpy(records[recordednum].date, date);
-        strcpy(records[recordednum].time, time);
-        records[recordednum].steps=atoi(steps);
-        recordednum++;
-    }
-        
-    fclose(file);
-
+    ImportedFile();
     printf("Total records: %d\n", recordednum);
 }
 
 void C_FewestSteps() {
-    first_1();
+    ImportedFile();
     int min;
-    int n;
     int Num;
 
     min = records[0].steps;
-    for (n = 0; n < i; n++){
-        if (records[n].steps < min){
-            min = records[n].steps;
-            Num = n;
+    for (i = 0; i < recordednum; i++){
+        if (records[i].steps < min){
+            min = records[i].steps;
+            Num = i;
         }
     }
-    printf("Fewest steps: %s, %s\n", records[Num].date, records[Num].time);
+    printf("Fewest steps: %s %s\n", records[Num].date, records[Num].time);
+}
+
+void D_LargestSteps() {
+    ImportedFile();
+    int Max;
+    int Num = 0;
+
+    Max = records[0].steps;
+    for (i = 1; i < recordednum; i++){
+        if (records[i].steps > Max){
+            Max = records[i].steps;
+            Num = i;
+        }
+    }
+    printf("Largest steps: %s %s\n", records[Num].date, records[Num].time);
 
 }
+
+
 int main() {
     char choice;
     printf("MENU: \n");
@@ -137,7 +151,7 @@ int main() {
 
         case 'D':
         case 'd':
-
+            D_LargestSteps();
             break;
 
         case 'E':
@@ -161,5 +175,5 @@ int main() {
 
         
     }
-   
+    
 }
