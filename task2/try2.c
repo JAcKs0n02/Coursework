55e91bdd0e484cc037d43126fcdeb225466192a3
Yifan Ren <146280681+JAcKs0n02@users.noreply.github.com>
2023-11-23 16:46:43 +0000

    task2

--- task2/try2.c
+++ task2/try2.c
@@ -45,8 +45,8 @@ void tokeniseRecord(const char *input, const char *delimiter,
 FITNESS_DATA records[100];
 int recordednum = 0, i;
 
-void ImportedFile() {
-    FILE *file = fopen("FitnessData_2023.csv", "r");
+void importFile(const char *filename) {
+    FILE *file = fopen(filename, "r");
     if (file == NULL) {
         fprintf(stderr, "Error: Unable to open the file.\n");
         return;
@@ -55,54 +55,40 @@ void ImportedFile() {
     char line[100];
     char date[11] , time[6], steps[5];
 
-    while(fgets(line,30,file)){
-    tokeniseRecord(line, ",", date, time, steps);
-    strcpy(records[recordednum].date, date);
-    strcpy(records[recordednum].time, time);
-    records[recordednum].steps=atoi(steps);
-    recordednum++;
+    while(fgets(line, 100, file)) {
+        tokeniseRecord(line, ",", date, time, steps);
+        strcpy(records[recordednum].date, date);
+        strcpy(records[recordednum].time, time);
+        records[recordednum].steps = atoi(steps);
+        recordednum++;
     }
    
     fclose(file);
 }
 
 void A_CheckFilename() {
-    ImportedFile();
-
-    char inputname[] = "FitnessData_2023.csv";
-    char CheckName[100];
+    importFile(const char *filename);
+    char inputname[100];
     printf("Enter the Filename: ");
-        scanf("%s", CheckName);
-        if (strcmp(CheckName, inputname)){
-            printf("Error: Could not open file\n");
-        }
-        else{
-            printf("File successfully loaded.\n");
-        }
+    scanf("%s", inputname);
+
+    importFile(inputname);
+
+    if (recordednum == 0) {
+        printf("Error: Could not find or open the file.\n");
+    } else {
+        printf("File successfully loaded.\n");
+    }
 }
 
+
 void B_TotalNum() {
-    ImportedFile();
+    importFile(const char *filename);
     printf("Total records: %d\n", recordednum);
 }
 
-void D_LargestSteps() {
-    ImportedFile();
-    int Max = records[0].steps;
-    int Num = 0;
-
-    for (i = 0; i < recordednum; i++){
-        if (records[i].steps > Max){
-            Max = records[i].steps;
-            Num = i;
-        }
-    }
-    printf("Largest steps: %s %s\n", records[Num].date, records[Num].time);
-
-}
-
 void C_FewestSteps() {
-    ImportedFile();
+    importFile(const char *filename);
     int min;
     int Num;
 
@@ -116,10 +102,24 @@ void C_FewestSteps() {
     printf("Fewest steps: %s %s\n", records[Num].date, records[Num].time);
 }
 
+void D_LargestSteps() {
+    importFile(const char *filename);
+    int Max;
+    int Num;
 
+    Max = records[0].steps;
+    for (i = 0; i < recordednum; i++){
+        if (records[i].steps > Max){
+            Max = records[i].steps;
+            Num = i;
+        }
+    }
+    printf("Largest steps: %s %s\n", records[Num].date, records[Num].time);
+
+}
 
 void E_MeanSteps (){
-    ImportedFile();
+    importFile(const char *filename);
     int totalSteps = 0;
 
     for (i = 0; i < recordednum; i++) {
@@ -127,11 +127,11 @@ void E_MeanSteps (){
     }
 
     float mean = (float)totalSteps / recordednum;
-    printf("Mean step count: %.0f\n", mean);
+    printf("Mean step count: %.2f\n", mean);
 }
 
-void F_StepsAbove500() {
-    ImportedFile();
+void F_LongestAbove500Steps() {
+    importFile(const char *filename);
     int longestCount = 0;
     int count = 0;
     int startIdx = 0;
@@ -159,8 +159,6 @@ void F_StepsAbove500() {
 
 int main() {
     char choice;
-    int fileLoaded = 0;
-
     printf("MENU: \n");
     printf("A: Specify the filename to be imported\n");
     printf("B: Display the total number of records in the file\n");
@@ -170,6 +168,7 @@ int main() {
     printf("F: Find the longest continuous period where the step count is above 500 steps\n");
     printf("Q: QUit\n");
     printf("\n");
+
 
     printf("Enter your choice: ");
     scanf("%c", &choice);
@@ -197,12 +196,12 @@ int main() {
 
         case 'E':
         case 'e':
-            E_MeanSteps();
+
             break;
 
         case 'F':
         case 'f': 
-            F_StepsAbove500();
+
             break;
 
         case 'Q':
